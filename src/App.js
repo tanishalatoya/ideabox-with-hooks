2ad7521a7513f45c4d94b958fc452db90f940bfe
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-30 14:24:52 -0600

    Render IdeaForm and IdeasContainer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import IdeaForm from './IdeaForm/IdeaForm';
+import IdeasContainer from './IdeasContainer/IdeasContainer';
 import './App.css';
 
 const App = () => {
   return (
     <main>
       <h1>Ideabox with Hooks</h1>
-
+      <IdeaForm />
+      <IdeasContainer />
     </main>
   )
 }

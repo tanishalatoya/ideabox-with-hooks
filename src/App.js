c266cc8f5be28704cebd197afe99f3d28a8976d8
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-30 14:31:05 -0600

    Add useState and pass in props to components being rendered

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import IdeaForm from './IdeaForm/IdeaForm';
 import IdeasContainer from './IdeasContainer/IdeasContainer';
 import './App.css';
 
 const App = () => {
+  const [ideas, addIdea] = useState([])
   return (
     <main>
       <h1>Ideabox with Hooks</h1>
-      <IdeaForm />
-      <IdeasContainer />
+      <IdeaForm addIdea={addIdea}/>
+      <IdeasContainer ideas={ideas} />
     </main>
   )
 }
